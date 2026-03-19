41a08ceee5e39ea11e66e4a11e056a954493e0a0
Grigoriy Nikitenko <grigonik22@gmail.com>
2026-03-19 13:04:28 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write code here
+const inputElements = [...document.querySelectorAll('input')];
+
+function enhanceFormInputs(inputs) {
+  const transformInputName = (input) => {
+    const toLower = input.name.toLowerCase();
+    const transformName = toLower.replace(/^./, (match) => match.toUpperCase());
+
+    return transformName;
+  };
+
+  inputs.forEach((input) => {
+    const label = document.createElement('label');
+    const inputName = input.name;
+    const inputId = input.id;
+    const placeholderName = transformInputName(input);
+
+    label.setAttribute('class', 'field-label');
+    label.setAttribute('for', inputId);
+    label.textContent = inputName;
+
+    input.before(label);
+
+    input.setAttribute('placeholder', placeholderName);
+  });
+}
+
+enhanceFormInputs(inputElements);
